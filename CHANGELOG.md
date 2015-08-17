53b4b284c7a8dc448cf958288e312c31b5e52084
Seth Fitzsimmons <seth@mojodna.net>
2015-08-17 12:15:52 -0700

    cartodb+file: protocol implementation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changes
 
+## v0.2.0 - 8/17/15
+
+* Implemented `cartodb+file:` protocol for instantiation of named map
+  configuration files
+
 ## v0.1.0 - 8/4/15
 
 * Initial version
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var assert = require("assert"),
+    path = require("path"),
     url = require("url"),
     util = require("util");
 
@@ -12,6 +13,33 @@ var meta = require("./package.json");
 
 debug = debug(meta.name);
 
+var makeRequest = function(method, options, callback) {
+  debug("%s %s", method.toUpperCase(), options.uri);
+  return request[method.toLowerCase()](options, function(err, rsp, body) {
+    if (err) {
+      return console.warn(err.stack);
+    }
+
+    switch (true) {
+    case rsp.statusCode === 200:
+      debug(rsp.statusCode, body);
+      return callback(null, rsp, body);
+
+    case rsp.statusCode >= 400 && rsp.statusCode < 500:
+      debug("we did something wrong (%d):", rsp.statusCode, body);
+      return callback(rsp);
+
+    case rsp.statusCode >= 500:
+      debug("CartoDB did something wrong (%d):", rsp.statusCode, body);
+      return callback(rsp);
+
+    default:
+      debug("something unexpected happened (%d):", rsp.statusCode, body);
+      return callback(rsp);
+    }
+  });
+};
+
 module.exports = function(tilelive) {
   var CartoDB = function(uri, callback) {
     if (typeof uri === "string") {
@@ -20,8 +48,8 @@ module.exports = function(tilelive) {
 
     var auth = (uri.auth || "").split(":", 2);
 
-    this.username = auth.shift() || process.env.CARTODB_USERNAME;
-    this.apiKey = auth.shift() || process.env.CARTODB_API_KEY;
+    this.username = auth.shift() || process.env.CARTODB_USERNAME || "";
+    this.apiKey = auth.shift() || process.env.CARTODB_API_KEY || "";
     this.hostname = uri.hostname || process.env.CARTODB_HOSTNAME || "cartodb.com";
     this.scale = (uri.query || {}).scale || 1;
 
@@ -44,8 +72,22 @@ module.exports = function(tilelive) {
         return tilelive.load(template, callback);
       });
 
-    // case "cartodb+file:":
-    //   break;
+    case "cartodb+file:":
+      var filename = path.resolve(path.join(uri.hostname, uri.pathname)),
+          config = require(filename),
+          self = this;
+
+      // override hostname, as it would have been set to a path fragment (or
+      // nothing)
+      this.hostname = process.env.CARTODB_HOSTNAME || "cartodb.com";
+
+      return self._createOrUpdate(config, function(err) {
+        if (err) {
+          return callback(err);
+        }
+
+        return tilelive.load(util.format("cartodb://%s:%s@%s/%s?scale=%d", self.username, self.apiKey, self.hostname, config.name, self.scale), callback);
+      });
 
     default:
       return callback(new Error(util.format("Protocol '%s' is not valid for %s", uri.protocol, meta.name)));
@@ -70,43 +112,51 @@ module.exports = function(tilelive) {
     }.bind(this));
   };
 
+  CartoDB.prototype._createOrUpdate = function(config, callback) {
+    var self = this;
+
+    return this._update(config, function(err, rsp, body) {
+      if (err && err.statusCode === 400) {
+        return self._create(config, callback);
+      }
+
+      return callback.apply(null, arguments);
+    });
+  };
+
+  CartoDB.prototype._create = function(config, callback) {
+    return makeRequest("post", {
+      qs: {
+        api_key: this.apiKey
+      },
+      json: config,
+      uri: util.format("https://%s.%s/api/v1/map/named", this.username, this.hostname)
+    }, callback);
+  };
+
+  CartoDB.prototype._update = function(config, callback) {
+    return makeRequest("put", {
+      qs: {
+        api_key: this.apiKey
+      },
+      json: config,
+      uri: util.format("https://%s.%s/api/v1/map/named/%s", this.username, this.hostname, config.name)
+    }, callback);
+  };
+
   CartoDB.prototype._instantiate = function(callback) {
-    return request.post({
+    return makeRequest("post", {
       qs: {
         api_key: this.apiKey
       },
       json: {},
       uri: util.format("https://%s.%s/api/v1/map/named/%s", this.username, this.hostname, this.name)
-    }, holdtime(function(err, rsp, body, elapsed) {
-      if (err) {
-        return callback(err);
-      }
-
-      debug("Instantiation took %dms", elapsed);
-
-      switch (true) {
-      case rsp.statusCode === 200:
-        debug(rsp.statusCode, body);
-        return callback(null, rsp, body);
-
-      case rsp.statusCode >= 400 && rsp.statusCode < 500:
-        debug("we did something wrong (%d):", rsp.statusCode, body);
-        return callback(rsp);
-
-      case rsp.statusCode >= 500:
-        debug("CartoDB did something wrong (%d):", rsp.statusCode, body);
-        return callback(rsp);
-
-      default:
-        debug("something unexpected happened (%d):", rsp.statusCode, body);
-        return callback(rsp);
-      }
-    }));
+    }, callback);
   };
 
   CartoDB.registerProtocols = function(tl) {
     tl.protocols["cartodb:"] = CartoDB;
-    // tl.protocols["cartodb+file:"] = CartoDB;
+    tl.protocols["cartodb+file:"] = CartoDB;
   };
 
   CartoDB.registerProtocols(tilelive);
